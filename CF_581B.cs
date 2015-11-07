41decc66edc01cf5b8208c6b6fca1e1bea1d4005
rstolliker <ryan.stolliker@gmail.com>
2015-11-07 00:09:30 -0800

    final submitted
    Accepted submission

--- CF_581B.cs
+++ CF_581B.cs
@@ -1,10 +1,36 @@
 // Solution to Codeforces 581 B : Luxourious houses
 using System;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
 
 class CF_581B
 {
 	public static void Main(string[] args)
 	{
-		n = int.Parse(Console.ReadLine());
+		// take input
+		int n = int.Parse(Console.ReadLine());
+		int[] houses = Array.ConvertAll (Console.ReadLine ().Split (), int.Parse); // magic
+
+		// setup
+		int[] result = new int[n];
+		int m = houses [n - 1];
+		result [n - 1] = 0; // No houses to right, always
+
+		// computations
+		for (int i = n - 2; i >= 0; i--)
+		{
+			int h = houses[i];
+			// result[i] = h <= m ? m - h + 1 : 0;	// Nice, but not as through
+			if (h <= m)
+			{
+				result [i] = m - h + 1;
+			} else {
+				result[i] = 0;
+				m = h;
+			}
+		}
+
+		// output
+		Console.WriteLine (string.Join (" ", result));
 	}
 }
