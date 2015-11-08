7deb36b9bce9e2f96fb6d3bf8e2034727aec9e6d
rstolliker <ryan.stolliker@gmail.com>
2015-11-07 18:11:19 -0800

    Solution to Project Euler problem 1 in C# recursiv

---
+++ Euler_1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace euler_1
+{
+	public class Euler_1
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine (sum35 (999));
+			Console.ReadLine(); // wait to close window
+		}
+
+		public static int sum35(int n)
+		{
+			if (n < 3)
+			{
+				return 0;
+
+			} else if((n % 3 == 0) || (n % 5 == 0))
+			{
+				return n + sum35 (n - 1);  // yay recursion
+			} else {
+				return sum35(n - 1);
+			}
+		}
+	}
+}
+
